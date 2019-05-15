0c3df3b4f19424290196cc8f2616e43d8a87a884
Wojciech Jasiński <ilikeheaps@gmail.com>
2019-05-15 23:55:56 +0200

    Generalize settings for MELPA (handling unsupported systems)

--- init.el
+++ init.el
@@ -240,8 +240,13 @@ Prefix argument N makes it go N lines down first."
 
 ;; settings for MELPA
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "This version of Emacs doesn't support SSL connections."))
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/")))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
