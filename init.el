bc02a29492d3925ff1baffcdb84c561f6fcf6156
Wojciech Jasiński <ilikeheaps@gmail.com>
2020-05-18 15:00:39 +0000

    Disable automatically added (package-initialize)

--- init.el
+++ init.el
@@ -16,6 +16,11 @@
 ;;;;; setting local variables while adhering to checkdoc is ugly
 ;;; Code:
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
 
 ;; provides =flet= (and more)
 (eval-when-compile (require 'cl))
